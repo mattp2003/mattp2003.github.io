97f6beff1b8adc9a60cfc57f0b1113e4506432e8
mattp2003 <96850801+mattp2003@users.noreply.github.com>
2021-12-30 00:04:50 -0800

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-function revealMessage() {
-    document.getElementById("process").style.display = "block";
-}
-
-function hideMessage() {
-    document.getElementById("process").style.display = "none";
-}
